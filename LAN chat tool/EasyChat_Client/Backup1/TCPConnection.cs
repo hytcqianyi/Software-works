9d3b7fe408f39983709afb72a3d6d8ba99472166
hytcqianyi <1163167478@qq.com>
2014-11-10 13:57:55 +0800

    2014。11.10
    局域网聊天软件

---
+++ LAN chat tool/EasyChat_Client/Backup1/TCPConnection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+
+namespace EasyChat
+{
+    /// <summary>
+    /// TCP连接类，成功连接返回TcpClient引用，否则返回null
+    /// </summary>
+    class TCPConnection
+    {
+        private IPAddress _ip = null;
+        private int _port;
+        private TcpClient _tcpc = null;//为 TCP 网络服务提供客户端连接
+
+        public TCPConnection(IPAddress ip, int port)
+        {
+            _ip = ip;
+            _port = port;
+        }
+
+        public TcpClient Connect()
+        {
+            try
+            {
+                _tcpc = new TcpClient();
+                _tcpc.Connect(_ip, _port);//连接到服务
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+            return _tcpc;
+        }
+    }
+}
---
+++ LAN chat tool/EasyChat_Server/StartItself.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+using Microsoft.Win32;
+namespace EasyChat_Server
+{
+    class StartItself
+    {
+        
+    }
+}
