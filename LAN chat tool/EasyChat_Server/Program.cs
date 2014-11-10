9378245341ab91e55338239995500e7daf1fd797
hytcqianyi <1163167478@qq.com>
2014-11-10 14:02:51 +0800

    2014.11.10
    局域网聊天工具

---
+++ LAN chat tool/EasyChat_Server/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+using Microsoft.Win32;
+namespace EasyChat_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Listener listener = new Listener();
+            
+            try
+            {
+                //listener.GetConfig();
+                listener.StartUp();
+            }
+            catch (Exception e)
+
+            {
+                Console.WriteLine("\n服务器发生异常,消息：" + e.Message);
+                listener.Close();
+            }
+        }
+        
+    }
+}
